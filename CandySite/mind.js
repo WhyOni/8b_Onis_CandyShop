efcf79894156dada28e1e164b1a018586bfef19c
Onis <110884574+WhyOni@users.noreply.github.com>
2023-04-19 06:45:48 +0000

    done today

--- CandySite/mind.js
+++ CandySite/mind.js
@@ -11,17 +11,26 @@ function signup() {
   const pas1 = document.getElementById("passOne").value;
   const pas2 = document.getElementById("passTwo").value;
   if (pas1.length >= 4 && pas1 === pas2) {
-    console.log("passed");
     adduser(pas1);
   } else {
     alert("credential information is incorrect");
   }
 
   function adduser(password) {
-    let fn = document.getElementById("firstname");
-    let ln = document.getElementById("lastname");
-    let email = document.getElementById("email");
+    let fn = document.getElementById("firstname").value;
+    let ln = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
     let newuser = { fn: fn, ln: ln, email: email, password: password };
     UserData.push(newuser);
+  }
+}
+
+function login() {
+  let email = document.getElementById("email").value;
+  let pass = document.getElementById("pass").value;
+
+  for (let i = 0; i < UserData.length; i++) {
+    if (UserData[i].email == email && UserData[i].password == pass) {
+    }
   }
 }
